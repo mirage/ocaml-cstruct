fb2448855cc5cdb0c6a94b8fcff57590b639317b
Anil Madhavapeddy <anil@recoil.org>
2012-05-10 12:26:12 +0100

    working pcap parser (for headers anyway)

--- lib_test/ipv4.ml
+++ lib_test/ipv4.ml
@@ -14,6 +14,24 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
+cstruct pcap_header {
+   uint32_t magic_number;   (* magic number *)
+   uint16_t version_major;  (* major version number *)
+   uint16_t version_minor;  (* minor version number *)
+   uint32_t thiszone;       (* GMT to local correction *)
+   uint32_t sigfigs;        (* accuracy of timestamps *)
+   uint32_t snaplen;        (* max length of captured packets, in octets *)
+   uint32_t network         (* data link type *)
+} as big_endian
+
+
+cstruct pcap_packet {
+  uint32_t ts_sec;         (* timestamp seconds *)
+  uint32_t ts_usec;        (* timestamp microseconds *)
+  uint32_t incl_len;       (* number of octets of packet saved in file *)
+  uint32_t orig_len        (* actual length of packet *)
+} as big_endian
+
 cstruct ipv4 {
    uint8_t        hlen_version;
    uint8_t        tos;
@@ -27,19 +45,25 @@ cstruct ipv4 {
    uint32_t       dst
 } as big_endian
 
-cstruct pcap_header {
-   uint32_t magic_number;   (* magic number *)
-   uint16_t version_major;  (* major version number *)
-   uint16_t version_minor;  (* minor version number *)
-   uint32_t thiszone;       (* GMT to local correction *)
-   uint32_t sigfigs;        (* accuracy of timestamps *)
-   uint32_t snaplen;        (* max length of captured packets, in octets *)
-   uint32_t network         (* data link type *)
-} as big_endian
 
 open Lwt
 open Printf
 
+let num_packets = ref 0
+
+let rec print_pcap_packet buf =
+  incr num_packets;
+  let hdr, rest = Cstruct.split buf sizeof_pcap_packet in
+  printf "** %lu.%lu  bytes %lu (of %lu)\n" 
+    (get_pcap_packet_ts_sec hdr)
+    (get_pcap_packet_ts_usec hdr)
+    (get_pcap_packet_incl_len hdr)
+    (get_pcap_packet_orig_len hdr);
+  let plen = Int32.to_int (get_pcap_packet_incl_len hdr)  in
+  let body, rest = Cstruct.split rest plen in
+  (* print_pcap_body body; *)
+  if Cstruct.len rest > 0 then print_pcap_packet rest
+  
 let print_pcap_header buf =
   let magic = get_pcap_header_magic_number buf in
   let endian =
@@ -48,6 +72,7 @@ let print_pcap_header buf =
     |0xd4c3b2a1l -> "littlendian"
     |_ -> "not a pcap file"
   in
+  printf "pcap_header (len %d)\n" sizeof_pcap_header;
   printf "magic_number %lx (%s)\n%!" magic endian;
   printf "version %d %d\n" 
    (get_pcap_header_version_major buf) (get_pcap_header_version_minor buf);
@@ -60,7 +85,11 @@ let parse () =
   lwt lfd = Lwt_unix.(openfile "http.cap" [O_RDONLY] 0) in
   let fd = Lwt_unix.unix_file_descr lfd in
   let buf = Lwt_bytes.map_file ~shared:false ~fd () in
-  print_pcap_header buf;
+  printf "total pcap file length %d\n" (Cstruct.len buf);
+  let header, packets = Cstruct.split buf sizeof_pcap_header in
+  print_pcap_header header;
+  print_pcap_packet packets;
+  printf "num_packets %d\n" !num_packets; 
   return ()
 
 let _ = Lwt_unix.run (parse ())
