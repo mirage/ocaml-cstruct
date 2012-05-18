4ecc939ebd5cf6e4248cdd4d64e775f206fdbc32
Anil Madhavapeddy <anil@recoil.org>
2012-05-18 16:51:34 +0100

    Add a shift_left function to pull a sub-view towards the base of the data.
    This can be used to expand a sub-view once it has been contracted. The function mutates an existing big-array and does *not* make a new one.

--- lib_test/basic.ml
+++ lib_test/basic.ml
@@ -28,8 +28,8 @@ cstruct bar {
   uint8_t d[8]
 } as big_endian
 
-
 let _ = 
+  (* Test basic set/get functions *)
   let be = Bigarray.(Array1.create char c_layout sizeof_foo) in
   for i = 0 to 255 do
     set_bar_a be i;
@@ -58,6 +58,7 @@ let _ =
       assert(get_bar_c be = i);
       fn (Int32.sub i 0x10l)
   in fn 0xffffffff_l;
+  (* Get/set buffers and blits *)
   let le = Bigarray.(Array1.create char c_layout sizeof_bar) in
   let rec fn = function
    |i when i < 0l -> ()
@@ -72,4 +73,20 @@ let _ =
   let sb1 = get_foo_d be in
   blit_bar_d sb1 0 le;
   assert(copy_bar_d le = s1);
-  Printf.printf "%s %s\n" (copy_foo_d be) (copy_bar_d le)
+  Printf.printf "%s %s\n" (copy_foo_d be) (copy_bar_d le);
+  (* Create sub-view and shift it back *)
+  let be = Bigarray.(Array1.create char c_layout sizeof_foo) in
+  set_foo_a be 7;
+  set_foo_b be 44;
+  set_foo_c be 0xbeef_l;
+  set_foo_d "abcdefgh" 0 be;
+  (* shifting the base array should fail *)
+  assert(not (Cstruct.shift_left be 1));
+  (* get a subview *)
+  let be2 = Cstruct.shift be 3 in
+  assert(Cstruct.BE.get_uint32 be2 0 = 0xbeef_l);
+  (* shift it back *)
+  assert(Cstruct.shift_left be2 3);
+  assert(Cstruct.BE.get_uint32 be2 3 = 0xbeef_l);
+  assert(get_foo_b be2 = 44);
+  assert(get_foo_a be2 = 7)
