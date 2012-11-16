7e63b4f99fed99dee37c887b190b9120d5f0e7c8
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-11-17 00:46:20 +0100

    First experiments with ocplib-endian

--- unix/lib/cstruct.ml
+++ unix/lib/cstruct.ml
@@ -17,7 +17,7 @@
 open Printf
 
 open Bigarray
-open Array1 
+open Array1
 
 type buf = (char, int8_unsigned_elt, c_layout) t
 
@@ -43,33 +43,33 @@ type uint64 = int64
 
 type ipv4 = int32
 
-let ipv4_to_string i =   
+let ipv4_to_string i =
   let (&&&) x y = Int32.logand x y in
-  let (>>>) x y = Int32.shift_right_logical x y in 
-  sprintf "%ld.%ld.%ld.%ld" 
+  let (>>>) x y = Int32.shift_right_logical x y in
+  sprintf "%ld.%ld.%ld.%ld"
     ((i &&& 0x0_ff000000_l) >>> 24) ((i &&& 0x0_00ff0000_l) >>> 16)
     ((i &&& 0x0_0000ff00_l) >>>  8) ((i &&& 0x0_000000ff_l)       )
-  
-let bytes_to_ipv4 bs = 
+
+let bytes_to_ipv4 bs =
   let (|||) x y = Int32.logor x y in
   let (<<<) x y = Int32.shift_left x y in
-  let a = Int32.of_int (byte_to_int bs.[0]) in 
-  let b = Int32.of_int (byte_to_int bs.[1]) in 
-  let c = Int32.of_int (byte_to_int bs.[2]) in 
-  let d = Int32.of_int (byte_to_int bs.[3]) in 
+  let a = Int32.of_int (byte_to_int bs.[0]) in
+  let b = Int32.of_int (byte_to_int bs.[1]) in
+  let c = Int32.of_int (byte_to_int bs.[2]) in
+  let d = Int32.of_int (byte_to_int bs.[3]) in
   (a <<< 24) ||| (b <<< 16) ||| (c <<< 8) ||| d
 
 type ipv6 = int64 * int64
-let ipv6_to_string (hi, lo) = 
+let ipv6_to_string (hi, lo) =
   let (&&&&) x y = Int64.logand x y in
   let (>>>>) x y = Int64.shift_right_logical x y in
   sprintf "%Lx:%Lx:%Lx:%Lx:%Lx:%Lx:%Lx:%Lx"
-    ((hi >>>> 48) &&&& 0xffff_L) ((hi >>>> 32) &&&& 0xffff_L) 
-    ((hi >>>> 16) &&&& 0xffff_L) ( hi          &&&& 0xffff_L) 
-    ((lo >>>> 48) &&&& 0xffff_L) ((lo >>>> 32) &&&& 0xffff_L) 
-    ((lo >>>> 16) &&&& 0xffff_L) ( lo          &&&& 0xffff_L) 
+    ((hi >>>> 48) &&&& 0xffff_L) ((hi >>>> 32) &&&& 0xffff_L)
+    ((hi >>>> 16) &&&& 0xffff_L) ( hi          &&&& 0xffff_L)
+    ((lo >>>> 48) &&&& 0xffff_L) ((lo >>>> 32) &&&& 0xffff_L)
+    ((lo >>>> 16) &&&& 0xffff_L) ( lo          &&&& 0xffff_L)
 
-let bytes_to_ipv6 bs = 
+let bytes_to_ipv6 bs =
   let (++++) x y = Int64.add x y in
   let (<<<<) x y = Int64.shift_left x y in
   let hihi = bytes_to_ipv4 (String.sub bs 0 4) in
@@ -78,18 +78,6 @@ let bytes_to_ipv6 bs =
   let lolo = bytes_to_ipv4 (String.sub bs 12 4) in
   ((Int64.of_int32 hihi) <<<< 48) ++++ (Int64.of_int32 hilo),
   ((Int64.of_int32 lohi) <<<< 48) ++++ (Int64.of_int32 lolo)
-
-let get_char s off =
-  get s off
-
-let get_uint8 s off =
-  Char.code (get s off)
-
-let set_char s off =
-  set s off
-
-let set_uint8 s off v =
-  set s off (Char.chr v)
 
 let sub_buffer src srcoff len =
   sub src srcoff len
@@ -111,65 +99,27 @@ let set_buffer src srcoff dst dstoff len =
     set dst (dstoff+i) src.[srcoff+i]
   done
 
+let set_uint8 = EndianBigstring.BigEndian_unsafe.set_int8
+let set_char = EndianBigstring.BigEndian_unsafe.set_char
+let get_uint8 = EndianBigstring.BigEndian_unsafe.get_uint8
+let get_char = EndianBigstring.BigEndian_unsafe.get_char
+
 module BE = struct
-
-  let get_uint16 s off =
-    let hi = get_uint8 s off in
-    let lo = get_uint8 s (off+1) in
-    (hi lsl 8) + lo
-
-  let get_uint32 s off =
-    let hi = get_uint16 s off in
-    let lo = get_uint16 s (off+2) in
-    Int32.(add (shift_left (of_int hi) 16) (of_int lo))
-
-  let get_uint64 s off =
-    let hi = get_uint32 s off in
-    let lo = get_uint32 s (off+4) in 
-    Int64.(add (shift_left (of_int32 hi) 32) (of_int32 lo))
-
-  let set_uint16 s off v =
-    set_uint8 s off (v lsr 8);
-    set_uint8 s (off+1) (v land 0xff)
-
-  let set_uint32 s off v =
-    set_uint16 s off (Int32.(to_int (shift_right_logical v 16)));
-    set_uint16 s (off+2) (Int32.(to_int (logand v 0xffff_l)))
-  
-  let set_uint64 s off v =
-    set_uint32 s off (Int64.(to_int32 (shift_right_logical v 32)));
-    set_uint32 s (off+4) (Int64.(to_int32 (logand v 0xffffffff_L)))
-
+  include EndianBigstring.BigEndian_unsafe
+  let set_uint16 = set_int16
+  let set_uint32 = set_int32
+  let get_uint32 = get_int32
+  let set_uint64 = set_int64
+  let get_uint64 = get_int64
 end
 
 module LE = struct
-
-  let get_uint16 s off =
-    let lo = get_uint8 s off in
-    let hi = get_uint8 s (off+1) in
-    (hi lsl 8) + lo
-
-  let get_uint32 s off =
-    let lo = get_uint16 s off in
-    let hi = get_uint16 s (off+2) in
-    Int32.(add (shift_left (of_int hi) 16) (of_int lo))
-
-  let get_uint64 s off =
-    let lo = get_uint32 s off in
-    let hi = get_uint32 s (off+4) in 
-    Int64.(add (shift_left (of_int32 hi) 32) (of_int32 lo))
-  
-  let set_uint16 s off v =
-    set_uint8 s off (v land 0xff);
-    set_uint8 s (off+1) (v lsr 8)
-
-  let set_uint32 s off v =
-    set_uint16 s off (Int32.(to_int (logand v 0xffff_l)));
-    set_uint16 s (off+2) (Int32.(to_int (shift_right_logical v 16)))
-
-  let set_uint64 s off v =
-    set_uint32 s off (Int64.(to_int32 (logand v 0xffffffff_L)));
-    set_uint32 s (off+4) (Int64.(to_int32 (shift_right_logical v 32)))
+  include EndianBigstring.LittleEndian_unsafe
+  let set_uint16 = set_int16
+  let set_uint32 = set_int32
+  let get_uint32 = get_int32
+  let set_uint64 = set_int64
+  let get_uint64 = get_int64
 end
 
 let len buf = dim buf
@@ -197,7 +147,7 @@ external shift_left : buf -> int -> bool = "caml_bigarray_shift_left"
 
 let sub buf off len = sub buf off len
 
-let to_string buf = 
+let to_string buf =
   let sz = len buf in
   let s = String.create sz in
   for i = 0 to sz - 1 do
