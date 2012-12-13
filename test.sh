91d2e14703d2318cc6b734cc6fba9b4469779b3d
Anil Madhavapeddy <anil@recoil.org>
2012-12-13 12:57:49 +0000

    fix test script

--- test.sh
+++ test.sh
@@ -1,20 +1,20 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
-endian=$(ocamlfind query ocplib-endian.bigstring -format "-I %d %a" -predicates native,archive)
+endian=`ocamlfind query ocplib-endian.bigstring -format "-I %d %a" -predicates native,archive)`
 
-function test {
+test() {
 echo $1
 mkdir -p _build/lib_test
 cp lib_test/$1.ml _build/lib_test/$1.ml
 camlp4orf -printer o _build/syntax/cstruct-syntax.cma lib_test/$1.ml > _build/lib_test/$1.gen.ml
 camlp4orf -printer o _build/syntax/cstruct-syntax.cma lib_test/$1.mli > _build/lib_test/$1.gen.mli
-ocamlopt -pp 'camlp4orf -printer o _build/syntax/cstruct-syntax.cma' -I _build/lib -i lib_test/$1.ml > _build/lib_test/$1.inferred.mli
+ocamlopt -pp 'camlp4orf -printer o _build/syntax/cstruct-syntax.cma' -I _build/lib -I _build/unix -i lib_test/$1.ml > _build/lib_test/$1.inferred.mli
 cp _build/lib_test/$1.inferred.mli _build/lib_test/$1.mli
 rm -f _build/lib_test/$1.cmi
 cd _build/lib_test
-ocamlopt -pp 'camlp4orf ../syntax/cstruct-syntax.cma' -I ../lib -c $1.mli
-ocamlopt -pp 'camlp4orf ../syntax/cstruct-syntax.cma' -I ../lib -c $1.ml
-ocamlopt -I ../lib $endian unix.cmxa bigarray.cmxa cstruct.cmxa $1.cmx -o $1.opt
+ocamlopt -pp 'camlp4orf ../syntax/cstruct-syntax.cma' -I ../lib -I ../unix -c $1.mli
+ocamlopt -pp 'camlp4orf ../syntax/cstruct-syntax.cma' -I ../lib -I ../unix -c $1.ml
+ocamlopt -I ../lib -I ../unix $endian unix.cmxa bigarray.cmxa cstruct.cmxa unix_cstruct.cmxa $1.cmx -o $1.opt
 time ./$1.opt
 #cp $1.gen.mli $1.mli
 #ocamlopt -pp 'camlp4orf ../syntax/cstruct-syntax.cma' -I ../lib -c $1.mli
