9801af35f2dbc80cc24565d271c8a96113d8b874
Anil Madhavapeddy <anil@recoil.org>
2013-09-21 19:33:25 -0400

    install time package in Travis

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,7 +4,7 @@ OPAM_PACKAGES="ocplib-endian lwt async"
 # Install OCaml and OPAM PPAs
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time
 export OPAMYES=1
 export OPAMVERBOSE=1
 
