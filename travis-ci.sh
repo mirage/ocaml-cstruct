1c22bf4c8df9ea56eb95c20d704257a329611fb1
Anil Madhavapeddy <anil@recoil.org>
2013-09-21 18:48:34 -0400

    Add experimental Travis setup

---
+++ travis-ci.sh
@@ -0,0 +1,14 @@
+# OPAM packages needed to build tests
+OPAM_PACKAGES="ocplib-endian"
+
+export OPAM_PACKAGES
+sudo apt-get install software-properties-common
+echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml opam
+export OPAMYES=1
+opam init 
+opam install ${OPAM_PACKAGES}
+eval `opam config -env`
+make 
+
