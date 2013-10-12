5a9686c2070804ea9b2145e2378f4a3cf42793ea
Anil Madhavapeddy <anil@recoil.org>
2013-10-12 22:54:52 +0100

    update README with more information about the generated functions
    closes #6

--- lib_test/pcap.mli
+++ lib_test/pcap.mli
@@ -4,3 +4,10 @@ cstruct pcap_packet {
   uint32_t incl_len;       (* number of octets of packet saved in file *)
   uint32_t orig_len        (* actual length of packet *)
 } as little_endian
+
+cstruct ethernet {
+  uint8_t        dst[6];
+  uint8_t        src[6];
+  uint16_t       ethertype
+} as big_endian
+
