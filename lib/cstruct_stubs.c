7f9c864d129883945eeb8fd13ed49f733c36a714
Your Name <you@example.com>
2017-05-11 17:24:05 +0200

    Expose pointer to Cstruct data

--- lib/cstruct_stubs.c
+++ lib/cstruct_stubs.c
@@ -27,6 +27,29 @@
 #include <caml/bigarray.h>
 
 CAMLprim value
+caml_get_pointer(value val_buf){
+  // val_buf should be a struct caml_ba_array (aka Cstruct.t.buffer)
+  // return: int option
+  struct caml_ba_array * bigarr_buf = Caml_ba_array_val(val_buf);
+
+
+  if ((CAML_BA_LAYOUT_MASK & bigarr_buf->flags) != CAML_BA_C_LAYOUT
+      || NULL != bigarr_buf->proxy)
+  {
+      // return None
+      return(Val_int(0));
+  }
+
+  value v = Val_long( (uintptr_t) Caml_ba_data_val(val_buf)) ;
+
+  CAMLparam1( v ) ;
+  CAMLlocal1( some ) ;
+  some = caml_alloc(1, 0);
+  Store_field( some, 0, v );
+  CAMLreturn( some );
+}
+
+CAMLprim value
 caml_blit_bigstring_to_string(value val_buf1, value val_ofs1, value val_buf2, value val_ofs2, value val_len)
 {
   memcpy(String_val(val_buf2) + Long_val(val_ofs2),
