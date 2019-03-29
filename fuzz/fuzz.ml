4b0333b466de098ce8edf1dde6a913b0d0545ef1
Thomas Leonard <talex5@gmail.com>
2019-03-29 16:13:45 +0000

    Extend the fuzz tests a little

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -7,7 +7,10 @@ let create x =
 
 let create_sub x start len =
   try
-    let c = Cstruct.create x in
+    let c = Cstruct.create_unsafe x in
+    for i = 0 to len - 1 do
+      Cstruct.set_uint8 c i i
+    done;
     Cstruct.sub c start len
   with Invalid_argument _ -> bad_test ()
 
@@ -22,11 +25,21 @@ let buffer = map [uint8] Bigarray.(Array1.create Char c_layout)
 
 let pp_cstruct f c = Format.pp_print_string f (Cstruct.debug c)
 
+let check_within ~base x =
+  check Cstruct.(base.off <= x.off);
+  check Cstruct.(base.off + base.len >= x.off + x.len);
+  check Cstruct.(x.len >= 0 && x.len <= base.len)
+
 let () =
   assert (Array.length Sys.argv = 2);   (* Prevent accidentally running in quickcheck mode *)
   add_test ~name:"blit" [cstruct; int; cstruct; int; int] (fun src srcoff dst dstoff len ->
       try Cstruct.blit src srcoff dst dstoff len
-      with Invalid_argument _ -> ()
+      with Invalid_argument _ ->
+        check (srcoff < 0 || srcoff > Cstruct.len src ||
+               dstoff < 0 || dstoff > Cstruct.len src ||
+               len < 0 ||
+               len > Cstruct.len src - srcoff ||
+               len > Cstruct.len dst - dstoff)
     );
   add_test ~name:"sexp" [buffer] (fun b ->
       b |> Cstruct_sexp.sexp_of_buffer |> Cstruct_sexp.buffer_of_sexp
@@ -51,48 +64,81 @@ let () =
       | () -> check in_range
       | exception Invalid_argument _ -> check (not in_range)
     );
-  add_test ~name:"sub" [cstruct; int; int] (fun c off len ->
-      Fmt.pr "sub %d %d\n%!" off len;
-      match Cstruct.sub c off len with
-      | sub -> check (Cstruct.len sub <= Cstruct.len c);
-      | exception Invalid_argument _ -> ()
+  add_test ~name:"sub" [cstruct; int; int] (fun base off len ->
+      match Cstruct.sub base off len with
+      | sub ->
+        check_within ~base sub;
+        check (Cstruct.len sub = len)
+      | exception Invalid_argument _ ->
+        check (off < 0 || len < 0 || off + len < 0 || off + len > Cstruct.len base)
     );
-  add_test ~name:"shift" [cstruct; int] (fun c off ->
-      match Cstruct.shift c off with
-      | sub -> check (Cstruct.len sub <= Cstruct.len c);
-      | exception Invalid_argument _ -> ()
+  add_test ~name:"shift" [cstruct; int] (fun base off ->
+      match Cstruct.shift base off with
+      | sub ->
+        check_within ~base sub;
+        check (Cstruct.len sub = max (Cstruct.len base - off) 0);
+      | exception Invalid_argument _ -> check (off < 0 || off > Cstruct.len base)
     );
-  add_test ~name:"copy" [cstruct; int; int] (fun c off len ->
-      match Cstruct.copy c off len with
-      | sub -> check (String.length sub <= Cstruct.len c);
-      | exception Invalid_argument _ -> ()
+  add_test ~name:"copy" [cstruct; int; int] (fun base off len ->
+      match Cstruct.copy base off len with
+      | x ->
+        check (String.length x = len);
+        check (String.equal x (Cstruct.sub base off len |> Cstruct.to_string))
+      | exception Invalid_argument _ ->
+        check (off < 0 || len < 0 || off + len < 0 || off + len > Cstruct.len base)
     );
   add_test ~name:"blit_from_bytes" [bytes; int; cstruct; int; int] (fun src srcoff dst dstoff len ->
       match Cstruct.blit_from_bytes src srcoff dst dstoff len with
-      | () -> ()
-      | exception Invalid_argument _ -> ()
+      | () -> check (Cstruct.equal (Cstruct.sub (Cstruct.of_bytes src) srcoff len)
+                                   (Cstruct.sub dst dstoff len))
+      | exception Invalid_argument _ ->
+        check (srcoff < 0 || srcoff > Bytes.length src ||
+               dstoff < 0 || dstoff > Bytes.length src ||
+               len < 0 ||
+               len > Bytes.length src - srcoff ||
+               len > Cstruct.len dst - dstoff)
     );
   add_test ~name:"blit_to_bytes" [cstruct; int; bytes; int; int] (fun src srcoff dst dstoff len ->
       match Cstruct.blit_to_bytes src srcoff dst dstoff len with
-      | () -> ()
+      | () -> check (Cstruct.equal (Cstruct.sub src srcoff len)
+                                   (Cstruct.sub (Cstruct.of_bytes dst) dstoff len))
+      | exception Invalid_argument _ ->
+        check (srcoff < 0 || srcoff > Cstruct.len src ||
+               dstoff < 0 || dstoff > Cstruct.len src ||
+               len < 0 ||
+               len > Cstruct.len src - srcoff ||
+               len > Bytes.length dst - dstoff)
+    );
+  add_test ~name:"memset" [cstruct; int; int] (fun c x i ->
+      guard (i >= 0 && i < Cstruct.len c);
+      Cstruct.memset c x;
+      check (Cstruct.get_uint8 c i = x land 0xff)
+    );
+  add_test ~name:"set_len" [cstruct; int] (fun base len ->
+      match Cstruct.set_len base len with
+      | x ->
+        (* check_within ~base x; *)   (* Maybe deprecate set_len extending the allocation? *)
+        check (Cstruct.len x >= 0);
+        check (Cstruct.len x <= Bigarray.Array1.dim (base.Cstruct.buffer));
+        check (Cstruct.len x = len)
       | exception Invalid_argument _ -> ()
     );
-  add_test ~name:"memset" [cstruct; int] (fun c x ->
-      Cstruct.memset c x
-    );
-  add_test ~name:"set_len" [cstruct; int] (fun c x ->
-      match Cstruct.set_len c x with
-      | c2 -> check (Cstruct.len c2 <= Cstruct.len c)
+  add_test ~name:"add_len" [cstruct; int] (fun base len ->
+      match Cstruct.add_len base len with
+      | x ->
+        check (Cstruct.len x >= 0);
+        check (Cstruct.len x <= Bigarray.Array1.dim (base.Cstruct.buffer));
+        check (Cstruct.len x = Cstruct.len base + len)
       | exception Invalid_argument _ -> ()
     );
-  add_test ~name:"add_len" [cstruct; int] (fun c x ->
-      match Cstruct.add_len c x with
-      | c2 -> check (Cstruct.len c2 <= Cstruct.len c)
-      | exception Invalid_argument _ -> ()
-    );
-  add_test ~name:"split" [cstruct; option int; int] (fun c start len ->
-      match Cstruct.split ?start c len  with
-      | c1, c2 -> check (Cstruct.len c2 <= Cstruct.len c && Cstruct.len c1 <= Cstruct.len c)
+  add_test ~name:"split" [cstruct; option int; int] (fun base start len ->
+      match Cstruct.split ?start base len  with
+      | c1, c2 ->
+        check_within ~base c1;
+        check_within ~base c2;
+        let start = match start with None -> 0 | Some x -> x in
+        check (Cstruct.equal (Cstruct.sub base start len) c1);
+        check (Cstruct.equal (Cstruct.shift base (start + len)) c2)
       | exception Invalid_argument _ -> ()
     );
   add_test ~name:"BE.set_uint64" [cstruct; int] (fun c off ->
@@ -105,13 +151,15 @@ let () =
       check (Cstruct.lenv cs >= 0)
     );
   add_test ~name:"copyv" [list cstruct] (fun cs ->
-      ignore (Cstruct.copyv cs)
+      check (String.equal (Cstruct.copyv cs) (Cstruct.concat cs |> Cstruct.to_string))
     );
   add_test ~name:"fillv" [list cstruct; cstruct] (fun src dst ->
       let copied, rest = Cstruct.fillv ~src ~dst in
       check (copied + Cstruct.lenv rest = Cstruct.lenv src);
+      (* OCaml tends to underestimate how much space bigarrays are using: *)
+      Gc.minor ()
     );
   add_test ~name:"concat" [list cstruct] (fun cs ->
-      let len = Cstruct.len (Cstruct.concat cs) in
-      check (len >= 0 && len >= Cstruct.lenv cs);
+      let x = Cstruct.concat cs in
+      check (Cstruct.len x = Cstruct.lenv cs)
     );
