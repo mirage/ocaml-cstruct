4ccd4053ba977593b3b09874a1e7382a74ce8965
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-04-15 13:41:33 +0200

    Add mention in cstruct_cap.ml{,i}

--- lib/cstruct_cap.mli
+++ lib/cstruct_cap.mli
@@ -1,5 +1,6 @@
 (*
- * Copyright (c) 2012-2014 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2012-2019 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2019 Romain Calascibetta <romain.calascibetta@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
