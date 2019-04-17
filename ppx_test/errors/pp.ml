f3ca658ce660527b4f28ef04b37518b8f1c5f607
Anil Madhavapeddy <anil@recoil.org>
2019-04-17 20:12:37 +0100

    fix tests on 4.06

--- ppx_test/errors/pp.ml
+++ ppx_test/errors/pp.ml
@@ -1,2 +1,6 @@
-let () = at_exit (fun () -> exit 0)
+(* we can just use exit from 4.07 onwards, but 4.06
+ * and earlier executed at_exit recursively *)
+external sys_exit : int -> 'a = "caml_sys_exit"
+let () = at_exit (fun () -> sys_exit 0)
+
 let () = Migrate_parsetree.Driver.run_main ()
