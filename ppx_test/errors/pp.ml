d696925a70a65dbc3316e77eaee28e1d7bee8623
Anil Madhavapeddy <anil@recoil.org>
2019-11-24 15:51:27 +0000

    Fix tests on 4.07+
    This sets the error style to short, so it should work on previous
    compilers as well
    
    Closes #263

--- ppx_test/errors/pp.ml
+++ ppx_test/errors/pp.ml
@@ -3,4 +3,8 @@
 external sys_exit : int -> 'a = "caml_sys_exit"
 let () = at_exit (fun () -> sys_exit 0)
 
+#if OCAML_VERSION >= (4,7,0)
+let () = Clflags.(error_style := Some Short)
+#endif
+
 let () = Migrate_parsetree.Driver.run_main ()
